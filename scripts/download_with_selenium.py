725ec0fda84b2026e83e863290f0b91420ee04c9
andynet <andrejbalaz001@gmail.com>
2020-02-21 14:07:10 +0100

    download_with_selenium v1.0

--- scripts/download_with_selenium.py
+++ scripts/download_with_selenium.py
@@ -4,10 +4,41 @@
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 import time
+import os
 
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.alert import Alert
+
+# %%
+def get_elements(browser):
+    wait = WebDriverWait(browser, 60)
+
+    next_button = wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//*[contains(@class, 'bcpr-navigate-right')]")
+            )
+        )
+
+    download_button = wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//*[contains(@class, 'bp-btn-primary')]")
+            )
+        )
+
+    filename = wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//*[contains(@class, 'shared-folder-preview-file-name')]")
+            )
+        ).text
+
+    url = browser.current_url
+
+    return (url, filename, download_button, next_button)
+
+
 # %%
 options = Options()
 options.headless = True
@@ -18,23 +49,39 @@ profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/oc
 browser = Firefox(firefox_profile=profile, options=options)
 
 # %%
-browser.get('https://byu.app.box.com/v/ProteinStructurePrediction/file/525413022614')
+# url = 'https://byu.app.box.com/v/ProteinStructurePrediction/file/525413022614'
+# url = 'https://byu.app.box.com/v/ProteinStructurePrediction/file/525406802922'
+# url = 'https://byu.app.box.com/v/ProteinStructurePrediction/file/525406139424'
+# url = 'https://byu.app.box.com/v/ProteinStructurePrediction/file/525418936666'
+url = 'https://byu.app.box.com/v/ProteinStructurePrediction/file/525402645504'
+filename = None
+n_errors = 0
+file = open('downloaded.log', 'w')
 
+browser.get(url)
 while True:
-    print(browser.current_url)
-    while (len(elements := browser.find_elements_by_tag_name('button')) < 40):
+    try:
+        state = 'done'
+        url, filename, download_button, next_button = get_elements(browser)
+        if not os.path.exists(f'/home/andyb/Downloads/{filename}'):
+            download_button.click()
+            state = 'downloading'
+        print(f'{url}\t{filename}\t{state}', file=file, flush=True)
+        next_button.click()
         time.sleep(5)
+        n_errors = 0
+    except TimeoutException:
+        n_errors += 1
+        print(f'TimeoutException({n_errors}) - refreshing page {url}')
+        browser.get(url)
+    except StaleElementReferenceException:
+        n_errors += 1
+        print(f'StaleElementReferenceException({n_errors}) - refreshing page {url}')
+        browser.get(url)
 
-    for element in elements[::-1]:
-        if element.text == 'Download':
-            break
-
-    element.click()
-
-    elements = browser.find_elements_by_xpath('//*[@title="Next File"]')
-    if len(elements) < 1:
+    if n_errors > 4:
         break
-    else:
-        elements[0].click()
 
+input("Press enter to exit the program")
+# %%
 browser.close()
